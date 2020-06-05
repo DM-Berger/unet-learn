5cfbfabd4653033085681d3dd971f6978425e801
Derek M Berger <dmberger.dev@gmail.com>
2020-06-05 17:54:53 -0400

    finish model building

--- model/unet.py
+++ model/unet.py
@@ -1,20 +1,11 @@
 import torch.nn as nn
 
 from torch import Tensor
-from torch.nn import (
-    BatchNorm3d,
-    Conv3d,
-    ConvTranspose3d as UpConv3d,
-    GroupNorm3d,
-    MaxPool3d,
-    ReLU,
-    Sequential,
-)
-from typing import Dict, List, Optional, Tuple, Union
+from torch.nn import Conv3d
 
 from model.conv import ConvUnit
-from model.encode import EncodeBlock, Encoder
-from model.decode import DecodeBlock, Decoder
+from model.encode import Encoder
+from model.decode import Decoder
 
 
 """
@@ -38,4 +29,45 @@ class JoinBlock(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         x = self.conv0(x)
         x = self.conv1(x)
+        return x
+
+
+class UNet3d(nn.Module):
+    """Build the classic Ronnberger 3D U-Net.
+
+    Parameters
+    ----------
+    initial_features: int
+        The number of channels / filters / features to extract in the very first
+        convolution. Sets the resulting sizes for the entire net.
+
+    depth: int
+        Encoder / decoder depth. E.g. a depth of 3 will results in 3 encoding
+        blocks (two convolution layers + downsample), one joining bottom later, and 3
+        decoding blocks (upsample + two convolution layers).
+
+    normalization: bool
+        If True, apply normalization (GroupNorm3D) after each non-upsampling or
+        non-downsampling convolution within an encoding or decoding block.
+    """
+
+    def __init__(
+        self,
+        initial_features: int = 32,
+        n_labels: int = 2,
+        depth: int = 3,
+        normalization: bool = True,
+    ):
+        self.encoder = Encoder(initial_features, depth, normalization)
+        self.joiner = JoinBlock(self.encoder, normalization)
+        self.decoder = Decoder(self.encoder, normalization)
+        self.segmenter = Conv3d(
+            in_channels=2 * initial_features, out_channels=n_labels, kernel_size=1, stride=1
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x, skips = self.encoder(x)
+        x = self.joiner(x)
+        x = self.decoder(x, skips)
+        x = self.segmenter(x)
         return x
